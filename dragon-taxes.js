a594084c37eb8c57ac7ff73b6f8a168b1a214fe7
walsh9 <walsh9@gmail.com>
2017-04-17 20:42:54 -0400

    Update libs and graphics

--- dragon-taxes.js
+++ dragon-taxes.js
@@ -15,8 +15,7 @@ WebFontConfig = {
 
 (function(d) {
   var wf = d.createElement('script'), s = d.scripts[0];
-  // wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
-  wf.src = 'lib/webfont.js';
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
   s.parentNode.insertBefore(wf, s);
 })(document);
 
