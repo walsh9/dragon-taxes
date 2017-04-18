d4172e9b0fc3011c6cd59c0888cb4d4d9bf3841a
walsh9 <walsh9@gmail.com>
2017-04-17 23:03:11 -0400

    use old drag methods

--- dragon-drop.js
+++ dragon-drop.js
@@ -1,7 +1,15 @@
 $(function() {
-    $( "article,#timer" ).draggable({
-        stack: "article,#timer",
-        cancel: "input",
-    })
+    $.fn.extend({
+        preventDisableSelection: function(){
+            return this.each(function(i) {
+                $(this).bind('mousedown.ui-disableSelection selectstart.ui-disableSelection', function(e) {
+                    e.stopImmediatePropagation();
+                });
+            });
+        }
+    });
+    $( "article" ).find("input").preventDisableSelection();
+    $( "article,#timer" ).draggable().mousedown(function() {
+        $(this).parent().append(this);
+    });
 });
-
